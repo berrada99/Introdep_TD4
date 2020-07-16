6d959496b17f19492e6f3bdeefe8ac896cf36d12
Hassan <hassan.berrada99@gmail.com>
2020-07-16 11:46:07 +0200

    Second commit

--- main.c
+++ main.c
@@ -1,4 +1,8 @@
 #include<stdio.h>
+int sub(int n1, int n2)
+{
+    return n1 - n2;
+}
 
 int add(int n1, int n2)
 {
@@ -7,6 +11,7 @@ int add(int n1, int n2)
 
 int main()
 {
-    printf("%i", add(2, 3));	
+    printf("%i\n", sub(2, 3));	
+    printf("%i\n", add(2, 3));	
     printf("Hello World\n");
 }
